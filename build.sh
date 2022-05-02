81747834a33116e22c73a33c6fd8e896b109992c
wangyong <wangyong.cloud@bytedance.com>
2022-05-02 18:57:03 +0800

    feat: add build bash

--- build.sh
+++ build.sh
@@ -1,4 +1,30 @@
-# aliyun, password: ks123
+#!/bin/bash
+
+if [ -f "build.zip" ];then
+	rm build.zip
+fi
+
+yarn build
+if [ $? -eq 0 ]; then
+  echo "build success"
+else
+  echo "build failed"
+fi
+
+echo 'zip...'
+zip -r ./build.zip ./build
+
+if [ $? -eq 0 ]; then
+  echo "zip success"
+else
+  echo "zip failed"
+fi
+
 scp build.zip jiangyan@47.93.85.187:/tmp/
-# hk, password: ks123
 scp build.zip keysafe@154.23.179.150:/tmp/
+
+# aliyun, password: ks123
+# scp build.zip jiangyan@47.93.85.187:/tmp/
+
+# hk, password: ks123
+# scp build.zip keysafe@154.23.179.150:/tmp/
